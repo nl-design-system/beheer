ff36fc80866aa5bc3ca2402d71afa7a939c174b5
Richard van Heuven van Staereling <richadr@users.noreply.github.com>
2026-02-20 11:55:16 +0100

    feat: add lib

--- scripts/checkout.sh
+++ scripts/checkout.sh
@@ -1,26 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script:
 #  - checks out the main branch of all repositories, and
 #  - pulls the latest changes
 
 SCRIPT_DIR=$(dirname "$0")
+# shellcheck disable=SC1091
+source "$SCRIPT_DIR/_lib.sh"
+
 DIRS=$(node "$SCRIPT_DIR/getRepos.mjs")
 
-get_main_branch() {
-  git remote show origin | sed -n '/HEAD branch/s/.*: //p'
-}
-
 for dir in $DIRS; do
-  echo "\033[1m$dir\033[0m"
+  echo "${BOLD}$dir${NC}"
 
-  if [ -d "$dir/.git" ]; then
+  if is_git_repo "$dir"; then
     # shellcheck disable=SC2164
     cd "$dir"
 
-    MAIN_BRANCH=$(get_main_branch "$dir")
-    git checkout "$MAIN_BRANCH" 2>/dev/null || echo "No main branch in $dir"
-    git pull --ff-only || echo "Failed in $dir"
+    MAIN_BRANCH=$(get_main_branch)
+    git checkout "$MAIN_BRANCH" 2>/dev/null || echo "Checkout failed in $dir"
+    git pull --ff-only || echo "Pull failed in $dir"
 
     # shellcheck disable=SC2103
     cd ..

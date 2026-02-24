9df0cdc84ee27e4cc4bf1a3aee80362f12399c7c
Richard van Heuven van Staereling <richadr@users.noreply.github.com>
2026-02-24 15:01:10 +0100

    refactor: use subshells so you don't have to do "cd .."
    Thanks [shellcheck](https://www.shellcheck.net/wiki/SC2103)

--- scripts/checkout.sh
+++ scripts/checkout.sh
@@ -14,17 +14,15 @@ for dir in $DIRS; do
   echo "${BOLD}$dir${NC}"
 
   if is_git_repo "$dir"; then
-    # shellcheck disable=SC2164
-    cd "$dir"
-
-    MAIN_BRANCH=$(get_main_branch)
-    git checkout "$MAIN_BRANCH" 2>/dev/null || echo "Checkout failed in $dir"
-    git pull --ff-only || echo "Pull failed in $dir"
-
-    # shellcheck disable=SC2103
-    cd ..
-    echo
+    (
+      cd "$dir" || exit
+      MAIN_BRANCH=$(get_main_branch)
+      git checkout "$MAIN_BRANCH" 2>/dev/null || echo "Checkout failed in $dir"
+      git pull --ff-only || echo "Pull failed in $dir"
+    )
   else
     echo "$dir is not a git repo"
   fi
+
+  echo
 done
